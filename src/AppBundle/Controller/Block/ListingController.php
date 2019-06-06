5ef7850529f9b585479816b364766ebd35f7b006
MasterKafei <MasterKafei59@gmail.com>
2019-06-06 21:38:38 +0200

    [Fix] Block listing

--- src/AppBundle/Controller/Block/ListingController.php
+++ src/AppBundle/Controller/Block/ListingController.php
@@ -14,7 +14,7 @@ class ListingController extends Controller
 {
     public function listBlockAction(BlockBusiness $business, Request $request, NodeBusiness $nodeBusiness, Yarm $yarm)
     {
-        $addressPorts = $request->request->get('address_ports');
+        $addressPorts = json_decode($request->request->get('address_ports'));
         $nodes = [];
         $excludeCurrent = false;
         $currentNode = $nodeBusiness->getCurrentNode();
@@ -31,5 +31,18 @@ class ListingController extends Controller
             return $this->json(array_merge($business->getAllBlocks($nodes), [$blocks]));
         }
         return $this->json($business->getAllBlocks($nodes));
+    }
+
+    public function downloadBlockChainAction(NodeBusiness $nodeBusiness, BlockBusiness $blockBusiness, Yarm $yarm)
+    {
+        $nodes = array_merge($nodeBusiness->getAllNodes(), [$nodeBusiness->getCurrentNode()]);
+        $chains = [$yarm->getRepository(Block::class)->findAll()];
+        foreach($nodeBusiness->getAllNodes() as $node) {
+            $chains = array_merge($chains, $blockBusiness->getBlocksFromNode($node, $nodes));
+        }
+
+        $chain = $blockBusiness->getConsensusBlock($chains);
+
+        return $this->json($chain);
     }
 }
